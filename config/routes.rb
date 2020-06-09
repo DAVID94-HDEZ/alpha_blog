69fa8f9f45cd37dd2022bb62d056d100bcad8409
DAVID HDEZ <david.hernandez@erion.mx>
2020-06-09 16:26:42 -0500

    Modulo 88. Construyendo un nuevo articulo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'#la ruta que nos lleva al destino deseado en este  caso, HOME
   get 'about', to: 'pages#about'
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index, :new, :create]
 end
