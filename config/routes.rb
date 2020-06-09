ffa680bfd895da9edcb2ca139d93e0c434305ee9
DAVID HDEZ <david.hernandez@erion.mx>
2020-06-09 14:57:00 -0500

    09/06/2020 Actualización

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'pages#home'#la ruta que nos lleva al destino deseado en este  caso, HOME
   get 'about', to: 'pages#about'
+  resources :articles, only: [:show]
 end
